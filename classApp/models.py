45926303d1465f9fee5526fc7f15734cc268e00e
XavierAmico <christianxamico@gmail.com>
2026-03-12 13:53:14 -0500

    Added College and Majors to Profile Model

--- classApp/models.py
+++ classApp/models.py
@@ -1,17 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+
+# Create your models here.
 
 class Profile(models.Model):
-	user = models.OneToOneField(User,on_delete=models.CASCADE)
-	bio = models.TextField(blank=True)
-	age = models.IntegerField(null=True, blank=True)
-	phone = models.TextField(blank = True)
-
-	def __str__(self):
-		return self.user.username
-
-
-#class UTRGV_event(models.Model):
-#	date = models.DateField(blank=True)
-#	location = model.TextField()
-#	cost = models.IntegerField()
+	user = models.OneToOneField(User, on_delete = models.CASCADE)
+	bio = models.TextField(blank = True)
+	
