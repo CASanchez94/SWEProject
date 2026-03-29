54c08e015121598b1690bc7a6101ec38cee5361d
XavierAmico <christianxamico@gmail.com>
2026-03-29 15:55:01 -0500

    Added .gitignore and removed cached files to prevent corruption later

--- classApp/models.py
+++ classApp/models.py
@@ -18,8 +18,20 @@ class Profile(models.Model):
 		Art     = "ART", "Art"
 		Biology = "BIO", "Biology"
 
-	user = models.OneToOneField(User,on_delete=models.CASCADE)
+	user = models.OneToOneField(
+		User,
+		on_delete=models.CASCADE,
+		related_name="profile"
+		)
+
+	profile_pic = models.ImageField(
+		upload_to="profile_pictures/",
+		blank = True,
+		null = True
+		)
+
 	bio = models.TextField(blank=True)
+
 	college = models.CharField(
 		max_length = 4,
 		choices = College.choices,
@@ -27,13 +39,13 @@ class Profile(models.Model):
 	)
 	major = models.CharField(
 		max_length = 4,
-		choices = Major.choices,
+		choices= Major.choices,
 		default= Major.Undecided,
 	)
 
 
 	def __str__(self):
-		return self.user.username
+		return self.user.username if self.user_id else "Profile"
 
 
 #class UTRGV_event(models.Model):
