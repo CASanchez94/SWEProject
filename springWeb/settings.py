0786a8e81a4965463d728bab7490546b6da7efcd
Chris Sanchez <test@yeehaw.com>
2026-03-31 09:32:25 -0500

    stable_profile page branch

--- springWeb/settings.py
+++ springWeb/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = 'static/'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
+
+# Media files (uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
